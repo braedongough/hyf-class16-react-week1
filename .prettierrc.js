b83b734e3aca5a672065e3a576a322f25eb90340
Braedon <braedongough@gmail.com>
2021-02-21 15:15:14 +0100

    Format code

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    bracketSpacing: false,
+    jsxBracketSameLine: false,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    arrowParens: 'always',
+}
